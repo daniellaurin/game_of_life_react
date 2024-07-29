8bf251abf8ebe89a4c63b3644754879c6f2c9a0c
Daniel Laurin <daniellaurin21@proton.me>
2024-07-29 16:56:49 -0400

    completed the the react tutorial

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # game_of_life_react
+
+
+links for reference 
+
+
+https://en.wikipedia.org/wiki/Conway's_Game_of_Life
+https://emergentuniverse.fandom.com/wiki/Conway's_Game_of_Life
+https://github.com/benawad/react-conway-game-of-life/tree/master
+
+
+
+### to build and open the react app
+
+https://medium.com/cleverprogrammer/twitter-clone-using-reactjs-the-ultimate-guide-74e2b2a06fa7
+
--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import { produce } from "immer";
 const numRows = 50;
 const numCols = 50;
 
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+];
+
+const generateEmptyGrid = () => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+
+  return rows;
+};
+
 function App() {
   const [grid, setGrid] = useState(() => {
-    const rows = [];
-    for (let i = 0; i < numRows; i++) {
-      rows.push(Array.from(Array(numCols), () => 0));
-    }
-
-    return rows;
+    return generateEmptyGrid();
   });
 
   const [running, setRunning] = useState(false);
@@ -25,7 +40,30 @@ function App() {
       return;
     }
     // simulate
-    setTimeout(runSimulation, 1000);
+    setGrid((g) => {
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let j = 0; j < numCols; j++) {
+            let neightbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newJ = j + y;
+              if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+                neightbors += g[newI][newJ];
+              }
+            });
+
+            if (neightbors < 2 || neightbors > 3) {
+              gridCopy[i][j] = 0;
+            } else if (g[i][j] === 0 && neightbors === 3) {
+              gridCopy[i][j] = 1;
+            }
+          }
+        }
+      });
+    });
+
+    setTimeout(runSimulation, 100);
   }, []);
 
   return (
@@ -33,9 +71,34 @@ function App() {
       <button
         onClick={() => {
           setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSimulation();
+          }
         }}
       >
         {running ? "stop" : "start"}
+      </button>
+      <button
+        onClick={() => {
+          const rows = [];
+          for (let i = 0; i < numRows; i++) {
+            rows.push(
+              Array.from(Array(numCols), () => (Math.random() > 0.5 ? 1 : 0))
+            );
+          }
+
+          setGrid(rows);
+        }}
+      >
+        random
+      </button>
+      <button
+        onClick={() => {
+          setGrid(generateEmptyGrid());
+        }}
+      >
+        clear
       </button>
 
       <div
@@ -45,19 +108,19 @@ function App() {
         }}
       >
         {grid.map((rows, i) =>
-          rows.map((col, k) => (
+          rows.map((col, j) => (
             <div
-              key={`${i}-${k}`}
+              key={`${i}-${j}`}
               onClick={() => {
                 const newGrid = produce(grid, (gridCopy) => {
-                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                  gridCopy[i][j] = grid[i][j] ? 0 : 1;
                 });
                 setGrid(newGrid);
               }}
               style={{
                 width: 20,
                 height: 20,
-                backgroundColor: grid[i][k] ? "pink" : undefined,
+                backgroundColor: grid[i][j] ? "pink" : undefined,
                 border: "solid 1px black",
               }}
             />
